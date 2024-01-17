734d2190b4eed8a5ca7e96a12c6fe60841bf7aa3
gegadiasamidze <gegadiasamidze2011@gmail.com>
2024-01-17 21:58:01 +0400

    “commit”

---
+++ day 1/lesson 2 .py
@@ -0,0 +1,19 @@
+name  =  "gega"
+#name არის ცვალებადი
+#name არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+#"gega" არის ცვლადისთვის მნიშვნელობა
+
+surname  =  "diasamidze"
+
+#print(name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი  ობიექტი
+
+name = "gega" #ეს არის სტრინგი str (string) ტიპის ცვლადი
+age = 22 # ეს არის int (integer) მთელი რიცხვი
+height = 184.5 #ეს არის float ტიპის ცვლადი ( ათწილადი)
+#boolean (bool) ტიპის ცვლადი
+
+knows_programming = True
+is_ugly = False  # snakecase(უბრალოდ წერის სტილი სტანდარტულად)
+
+isUgly = False # ჯავასკრიპტული camelcese
---
+++ day 1/lesson1.py
@@ -0,0 +1,6 @@
+from turtle import *
+
+print("nika keshelava")
+print(5)  #integer #int
+print("5")   #string  #str
+color("gega diasamidze" + "aris 12 wlis")
